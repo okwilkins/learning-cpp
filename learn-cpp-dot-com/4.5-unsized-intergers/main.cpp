845fdaba88907a8c20b79272817613f0941287c9
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-18 19:30:44 +0000

    feat: 4.5

---
+++ learn-cpp-dot-com/4.5-unsized-intergers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void doSomething(unsigned int x) {
+  // Run some code x times
+
+  std::cout << "x is " << x << '\n';
+}
+
+int main() {
+  doSomething(-1);
+
+  return 0;
+}
