fbd2b2bbc1bd0ce931455a863afb5fe085498235
Oliver Wilkins <okwilkins@googlemail.com>
2026-01-25 21:54:40 +0000

    feat: 12.x

---
+++ learn-cpp-dot-com/12.x/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+
+void sort2(int &a, int &b) {
+    if (a > b) {
+        std::swap(a, b);
+    }
+}
+
+int main() {
+    int x{7};
+    int y{5};
+
+    std::cout << x << ' ' << y << '\n'; // should print 7 5
+
+    sort2(x, y);                        // make sure sort works when values need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    sort2(x, y);                        // make sure sort works when values don't need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    return 0;
+}
